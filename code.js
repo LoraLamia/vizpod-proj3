ad0a6d63dfe99556f2928efa4a3dd9219eb52a69
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2024-01-23 17:52:09 +0100

    box plot added

--- code.js
+++ code.js
@@ -1,3 +1,92 @@
+
+function createBoxPlot(data) {
+    const margin = { top: 10, right: 30, bottom: 40, left: 60 },
+          width = 800 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("#box-plot").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map(d => d.employment_type))
+        .paddingInner(1)
+        .paddingOuter(.5);
+    
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.max)])
+        .range([height, 0]);
+
+        svg.append("g").call(d3.axisLeft(y).tickFormat(d3.format("~s")));
+    svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)") 
+        .attr("y", 0 - margin.left - 45) 
+        .attr("x",0 - (height / 2)) 
+        .attr("dy", "3em") 
+        .style("text-anchor", "middle")
+        .text("Salary");
+
+        svg.append("text")
+        .attr("x", width / 2) 
+        .attr("y", height + margin.bottom - 5) 
+        .style("text-anchor", "middle") 
+        .text("Employment type"); 
+
+
+    const boxWidth = 100;
+    data.forEach(d => {
+
+        svg.append("rect")
+            .attr("x", x(d.employment_type) - boxWidth/2)
+            .attr("y", y(d.q3))
+            .attr("height", y(d.q1) - y(d.q3))
+            .attr("width", boxWidth)
+            .attr("stroke", "black")
+            .style("fill", "#69b3a2");
+
+        svg.append("line")
+            .attr("x1", x(d.employment_type) - boxWidth/2)
+            .attr("x2", x(d.employment_type) + boxWidth/2)
+            .attr("y1", y(d.median))
+            .attr("y2", y(d.median))
+            .attr("stroke", "black");
+        
+        svg.append("line")
+            .attr("x1", x(d.employment_type))
+            .attr("x2", x(d.employment_type))
+            .attr("y1", y(d.min))
+            .attr("y2", y(d.q1))
+            .attr("stroke", "black");
+        
+        svg.append("line")
+            .attr("x1", x(d.employment_type))
+            .attr("x2", x(d.employment_type))
+            .attr("y1", y(d.q3))
+            .attr("y2", y(d.max))
+            .attr("stroke", "black");
+
+        svg.append("line")
+            .attr("x1", x(d.employment_type) - boxWidth/4)
+            .attr("x2", x(d.employment_type) + boxWidth/4)
+            .attr("y1", y(d.min))
+            .attr("y2", y(d.min))
+            .attr("stroke", "black");
+        
+        svg.append("line")
+            .attr("x1", x(d.employment_type) - boxWidth/4)
+            .attr("x2", x(d.employment_type) + boxWidth/4)
+            .attr("y1", y(d.max))
+            .attr("y2", y(d.max))
+            .attr("stroke", "black");
+    });
+}
+
+
 d3.csv("ds_salaries.csv").then(data => {
     data.forEach(d => {
         d.salary_in_usd = +d.salary_in_usd;
@@ -9,7 +98,7 @@ d3.csv("ds_salaries.csv").then(data => {
 
     const salaryByJob = d3.rollups(data, v => d3.mean(v, d => d.salary_in_usd), d => d.job_title);
 
-    const margin = {top: 30, right: 50, bottom: 200, left: 100};
+    const margin = { top: 30, right: 50, bottom: 200, left: 100 };
 
     const barChartWidth = 1000, barChartHeight = 800;
     const xScale = d3.scaleBand()
@@ -36,10 +125,10 @@ d3.csv("ds_salaries.csv").then(data => {
         .text("Average Salary (USD)");
 
     barChartSvg.append("text")
-    .attr("class", "axis-label")
-    .attr("transform", "translate(" + (barChartWidth / 2) + "," + (barChartHeight + margin.bottom - 170) + ")")
-    .style("text-anchor", "middle")
-    .text("Job Description");
+        .attr("class", "axis-label")
+        .attr("transform", "translate(" + (barChartWidth / 2) + "," + (barChartHeight + margin.bottom - 170) + ")")
+        .style("text-anchor", "middle")
+        .text("Job Description");
 
     const updateChart = (experienceLevel) => {
         let filteredData = experienceLevel === "ALL" ? data : data.filter(d => d.experience_level === experienceLevel);
@@ -82,19 +171,16 @@ d3.csv("ds_salaries.csv").then(data => {
 
         bars.exit().remove();
 
-        barChartSvg.selectAll(".x-axis").data([0]).enter().append("g").attr("class", "x-axis");
-        barChartSvg.selectAll(".y-axis").data([0]).enter().append("g").attr("class", "y-axis");
-
-        // barChartSvg.selectAll(".x-axis")
-        //     .attr("transform", "translate(0," + barChartHeight + ")")
-        //     .call(d3.axisBottom(xScale))
-        //     .selectAll("text")
-        //     .attr("text-anchor", "end")
-        //     .attr("transform", "rotate(-45)")
-        //     .style("font-size", "16px")
-        //     .attr("dx", ".8em")
-        //     .attr("dy", "-.35em");
-
+        barChartSvg.selectAll(".x-axis")
+            .data([0])
+            .enter()
+            .append("g")
+            .attr("class", "x-axis");
+        barChartSvg.selectAll(".y-axis")   
+            .data([0])
+            .enter()
+            .append("g")
+            .attr("class", "y-axis");
         barChartSvg.selectAll(".y-axis")
             .data([0])
             .join("g")
@@ -104,7 +190,7 @@ d3.csv("ds_salaries.csv").then(data => {
             .style("font-size", "14px");
     };
 
-    const lineChartMargin = {top: 10, right: 50, bottom: 50, left: 100};
+    const lineChartMargin = { top: 10, right: 50, bottom: 50, left: 100 };
     const lineChartWidth = 960 - lineChartMargin.left - lineChartMargin.right;
     const lineChartHeight = 500 - lineChartMargin.top - lineChartMargin.bottom;
 
@@ -198,7 +284,7 @@ d3.csv("ds_salaries.csv").then(data => {
 
     const locations = Array.from(new Set(data.map(d => d.company_location)));
 
-    const marginB = {top: 30, right: 50, bottom: 130, left: 100};
+    const marginB = { top: 30, right: 50, bottom: 130, left: 100 };
 
     const svgWidth = 1300;
     const svgHeight = 768;
@@ -345,4 +431,21 @@ d3.csv("ds_salaries.csv").then(data => {
             d3.select(this)
                 .style("fill", d => d3.schemeCategory10[d.index % 10]);
         });
+
+
+    const groupedData = d3.group(data, d => d.employment_type);
+    const boxPlotData = Array.from(groupedData, ([employmentType, values]) => {
+        const salaries = values.map(d => d.salary_in_usd).sort(d3.ascending);
+        const q1 = d3.quantile(salaries, .25);
+        const median = d3.quantile(salaries, .5);
+        const q3 = d3.quantile(salaries, .75);
+        const interQuantileRange = q3 - q1;
+        const min = d3.min(values, d => d.salary_in_usd);
+        const max = d3.max(values, d => d.salary_in_usd);
+        return { employment_type: employmentType, q1, median, q3, min, max };
+    });
+
+    createBoxPlot(boxPlotData);
+
+
 });
